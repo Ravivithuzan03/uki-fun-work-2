cad57f394b52d6b7be2064bee1e86aaa69047a57
Ravivithuzan03 <ravivithuzan03@gamil.com>
2020-08-16 13:24:39 +0530

    uki-fun-work-2

---
+++ main.js
@@ -0,0 +1,191 @@
+SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function() { 
+      
+    SC.stream('/tracks/34580939',function(sound){
+        //function start
+        $('#mariyan').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          //function stop
+          $('#mariyan3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+ 
+     });
+   });   
+
+   SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function() { 
+      
+    SC.stream('/tracks/63137286',function(sound){
+        //function start
+        $('#i').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          //function stop
+          $('#i3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+ 
+     });
+   }); 
+   SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function() { 
+      
+    SC.stream('/tracks/34581593',function(sound){
+        //function start
+        $('#jodhaa').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          //function stop
+          $('#jodhaa3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+ 
+     });
+   });   
+   SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function() { 
+      
+    SC.stream('/tracks/28544446',function(sound){
+        //function start
+        $('#remo').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          //function stop
+          $('#remo3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+ 
+     });
+   });   
+   SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function() { 
+      
+    SC.stream('/tracks/31796988',function(sound){
+        //function start
+        $('#naanum').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          //function stop
+          $('#naanum3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+ 
+     });
+   });   
+   SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function() { 
+      
+    SC.stream('/tracks/11201079',function(sound){
+        //function start
+        $('#david').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          //function stop
+          $('#david3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+ 
+     });
+   });   
+   SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function() { 
+      
+    SC.stream('/tracks/68982672',function(sound){
+        //function start
+        $('#kappan').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          //function stop
+          $('#kappan3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+ 
+     });
+   });   
+   SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function() { 
+      
+    SC.stream('/tracks/16098608',function(sound){
+        //function start
+        $('#yaan').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          //function stop
+          $('#yaan3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+ 
+ 
+     });
+   });   
+   SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function() { 
+      
+    SC.stream('/tracks/99649312',function(sound){
+        //function start
+        $('#anniyan').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          //function stop
+          $('#anniyan3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+
+          
+    });
+   });  
+ 
